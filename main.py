e92a5a18467620603e655d2402417a22b6e357a4
Kiren <147826802+kirenj@users.noreply.github.com>
2024-03-29 10:18:14 +0000

    update

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import pandas
 
 data = pandas.read_csv("./nato_phonetic_alphabet.csv")
 
-user_input = input("Enter a word: ").upper()
-user_word_list = [i for i in user_input]
 phone_dictionary = {row["letter"]:row["code"] for (index, row) in data.iterrows()}
+
+def func():
+  user_input = input("Enter a word: ").upper()
+  user_word_list = [i for i in user_input]
+  
+  try:   
+    final_list = [phone_dictionary[i] for i in user_word_list]      
+  except KeyError:
+    print("Enter a valid word")
+    func()
+  else:
+    print(final_list)
+        
+func()
+
 
 # for (index,row) in data.iterrows():
 #     key = row["letter"]
 #     value = row["code"]
 #     letters[key] = value
-
-final_list = [phone_dictionary[i] for i in user_word_list for j in phone_dictionary if i == j]
-print(final_list)
